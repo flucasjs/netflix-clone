45888139e3f4ef06ae6478b6dec8e45852d622ee
unknown <flucasjs@gmail.com>
2023-05-08 22:51:14 -0700

    Rework initial project structure

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Aliqua officia eu dolore aliqua voluptate incididunt. Eiusmod eu ipsum
+      officia enim incididunt. Anim aute exercitation deserunt ipsum sunt sunt
+      labore consectetur do deserunt reprehenderit est incididunt commodo.
+      Mollit consequat adipisicing proident sit qui in sint Lorem duis non amet
+      duis nulla. Enim anim nostrud ipsum veniam sit voluptate ex. Occaecat
+      labore adipisicing aute dolore ex aliqua aute id ea qui nostrud sit. Ut
+      esse amet do ut reprehenderit exercitation exercitation reprehenderit
+      nostrud aliqua enim nulla nisi.
+    </div>
+  );
+}
+
+export default App;
