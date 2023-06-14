10d2aee62daa3965067bb5d700a68858f454d208
unknown <flucasjs@gmail.com>
2023-06-13 21:05:19 -0700

    Add global styles and reset CSS using normalize

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import 'normalize.css';
+import GlobalStyles from './global-styles';
 
 createRoot(document.getElementById('root'))
-  .render(<App />);
+  .render(
+    <>
+      <GlobalStyles />
+      <App />
+    </>,
+  );
