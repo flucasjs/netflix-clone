e6f492d5bc92d179987bd3d87632b378cac541cb
unknown <flucasjs@gmail.com>
2023-06-13 21:19:06 -0700

    Merge exported containers in index.js

---
+++ src/containers/index.js
@@ -0,0 +1,3 @@
+export { default as FaqsContainer } from './FaqsContainer';
+export { default as FooterContainer } from './FooterContainer';
+export { default as JumbotronContainer } from './JumbotronContainer';
