104ab786fd50610c3d82b2930312960c9ca026ed
unknown <flucasjs@gmail.com>
2023-06-25 20:56:22 -0700

    Add opt-form component to accordian

--- src/containers/FaqsContainer.jsx
+++ src/containers/FaqsContainer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Accordion } from '../components';
 import { faqsData } from '../fixtures';
+import OptForm from '../components/opt-form';
 
 function FaqsContainer() {
   return (
@@ -12,6 +13,16 @@ function FaqsContainer() {
           <Accordion.Body>{item.body}</Accordion.Body>
         </Accordion.Item>
       ))}
+
+      <OptForm>
+        <OptForm.Input placeholder="Email address" />
+        <OptForm.Button>Try it now</OptForm.Button>
+        <OptForm.Break />
+        <OptForm.Text>
+          Ready to watch? Enter your email to create or restart your
+          membership
+        </OptForm.Text>
+      </OptForm>
     </Accordion>
   );
 }
